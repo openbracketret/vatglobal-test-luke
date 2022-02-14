774fca3fcbc5e47dcd03f4f0f405f9cfa62d7e73
Luke Greenberg <lukegreen.dev@gmail.com>
2022-02-14 16:54:43 +0200

    init commit

---
+++ vatglobal_test/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from vatglobal_test.users.models import User
+from vatglobal_test.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
