7078138402494583fa6306999ac8cad238a0e7cb
Luke Greenberg <lukegreen.dev@gmail.com>
2022-02-15 16:06:08 +0200

    add tool test class

--- fileproc/tests.py
+++ fileproc/tests.py
@@ -1,3 +1,16 @@
+import pytest
 from django.test import TestCase
 
 # Create your tests here.
+from fileproc.tools import check_date_column_formatting
+
+pytestmark = pytest.mark.django_db
+
+class TestTools:
+    """
+    Testing class for all the functions inside of tools.py
+    """
+
+    def test_check_date_column(self):
+        assert check_date_column_formatting("2020/01/03") == False
+        assert check_date_column_formatting("12-12/12") == True
